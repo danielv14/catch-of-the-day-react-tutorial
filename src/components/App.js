a7ff7e7e42fd437e42d91f1269e6d683995f40af
Daniel Vernberg <daniel.vernberg@gmail.com>
2018-01-25 13:12:50 +0100

    lesson #16. Updating order state

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     super();
     this.addFish = this.addFish.bind(this);
     this.loadSamples = this.loadSamples.bind(this);
-
+    this.addToOrder = this.addToOrder.bind(this);
     // initial state
     this.state = {
       fishes: {},
@@ -36,6 +36,15 @@ class App extends React.Component {
       fishes: sampleFishes
     })
   }
+
+  addToOrder(key) {
+    // take copy of state
+    const order = {...this.state.order};
+    // update or add the new number of fish ordered
+    order[key] = order[key] + 1 || 1;
+    // update state
+    this.setState({ order });
+  }
   
   render() {
     return (
@@ -46,11 +55,14 @@ class App extends React.Component {
             {
               Object
                 .keys(this.state.fishes)
-                .map(key => <Fish key={key} details={this.state.fishes[key]} />)
+                .map(key => <Fish key={key} 
+                  details={this.state.fishes[key]} 
+                  addToOrder={this.addToOrder} 
+                  index={key} />)
             }
           </ul>
         </div>
-        <Order/>
+        <Order />
         <Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
       </div>
     )
