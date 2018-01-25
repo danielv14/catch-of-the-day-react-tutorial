5c65993a5b3ae6ecededa20369932d006f2051d5
Daniel Vernberg <daniel.vernberg@gmail.com>
2018-01-25 11:12:11 +0100

    lesson #10. Working with react events

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -3,12 +3,21 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  goToStore(event) {
+    event.preventDefault();
+    
+    console.log(this.storeInput.value);
+
+    // transition from / to /store/:storeID
+  }
+  
   render() {
     return (
-      <form  className="store-selector">
+      <form  className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
         <input type="text" required placeholder="Store Name"
-          defaultValue={getFunName()} />
+          defaultValue={getFunName()} ref={(input) => {this.storeInput = input}} />
         <button type="submit">Visit Store </button>
       </form>
     )
